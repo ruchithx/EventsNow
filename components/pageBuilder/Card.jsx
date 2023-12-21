2b24f45266599015db208e3f8c6137abb689b5d7
ruchith <ruchithsmarawickrama.sg@gmail.com>
2023-12-21 07:17:52 +0530

    initial component

--- components/pageBuilder/Card.jsx
+++ components/pageBuilder/Card.jsx
@@ -3,21 +3,25 @@ import { Text } from "./Text";
 import { Button } from "./Button";
 import { Container } from "./Container";
 import { useNode, Element } from "@craftjs/core";
+
+import { Editor, Frame } from "@craftjs/core";
 export const Card = ({ background, padding = 20 }) => {
   return (
     <Container background={background} padding={padding}>
-      <div className="text-only">
-        <Text text="Title" fontSize={20} />
-        <Text text="Subtitle" fontSize={15} />
-      </div>
-      <div className="buttons-only">
-        <Button
-          size="small"
-          text="Learn more"
-          variant="contained"
-          color="primary"
-        />
-      </div>
+      <Editor resolver={{ Card, Button, Text, Container }}>
+        <Element id="text" canvas>
+          <Text text="Title" fontSize={20} />
+          <Text text="Subtitle" fontSize={15} />
+        </Element>
+        <Element id="buttons" canvas>
+          <Button
+            size="small"
+            text="Learn more"
+            variant="contained"
+            color="primary"
+          />
+        </Element>
+      </Editor>
     </Container>
   );
 };
