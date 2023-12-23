6e786c68690101bda3a3da19c89e30d214e049e5
ashandilz <ashandilsara8@gmail.com>
2023-12-23 21:45:26 +0530

    disabled event card done

--- app/page.tsx
+++ app/page.tsx
@@ -1,55 +1,59 @@
-
-import ProfileCard from '@/components/ProfileCard';
-import EventCardEventDash from '../components/EventCardEventDash'
+import ProfileCard from "@/components/ProfileCard";
+import EventCardEventDash from "../components/EventCardEventDash";
 import BUyAdd from "@/components/BUyAdd";
 import CreateAccount from "@/components/CreateAccount";
 import Footer from "@/components/Footer";
 import InviteButton from "@/components/InviteButton";
 import Login from "@/components/Login";
 import Searchbar from "@/components/Searchbar";
+import EventCardDisabled from "../components/EventCardDisabled";
 
 // import Post from "@/components/Post";
 import EventCardOrgDash from "@/components/EventCardOrgDash";
 import EventCard from "@/components/EventCard";
 
-
 export default function Home() {
   return (
     <div>
-        <EventCard
+      <EventCardDisabled
+        img="image 3.png"
+        name="MEGA"
+        location="MATARA    "
+        date="21st MAY "
+      />
+      <EventCard
         img="image 3.png"
         name="MEGA"
         location="MATARA    "
         date="21st MAY "
         time="21.00"
-
       />
 
-<EventCardOrgDash
+      <EventCardOrgDash
         img="image 1.png"
         name="NADA GAMA "
         location="KCC,Kandy Road,kandy."
         date="21st June 2023"
         time="16.00 to 22.00"
       />
-    
 
-      <EventCardEventDash 
-      img="image 1.png"
-      name="NADA GAMA "
-      location="KCC,Kandy Road,kandy."
-      date="21st June 2023"
-      time="16.00 to 22.00"
-      buttonName="Stein Studios"/>
+      <EventCardEventDash
+        img="image 1.png"
+        name="NADA GAMA "
+        location="KCC,Kandy Road,kandy."
+        date="21st June 2023"
+        time="16.00 to 22.00"
+        buttonName="Stein Studios"
+      />
 
-      <ProfileCard 
+      <ProfileCard
         profilePic="profile.jpg"
         name="A-shan Dilz"
-        accountType='Organization account'
-        email='ashandilsara8@gmail.com'/>
+        accountType="Organization account"
+        email="ashandilsara8@gmail.com"
+      />
 
-      
-{/* 
+      {/* 
       <Login
         bgcolors={"bg-custom-orange"}
         titleOfbutton={"LOGOUT"}
@@ -84,10 +88,6 @@ export default function Home() {
         caption="Beautiful days"
         post="post"
       /> */}
- 
-
-
-
     </div>
   );
 }
---
+++ components/EventCardDisabled.tsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import Image from "next/image";
+
+interface EventCardDisabled {
+    name: string;
+    img: string;
+    location: string;
+    date: string;
+  }
+
+export default function EventCardDisabled({ name, img, location, date }: EventCardDisabled) {
+  return (
+    <div>
+      <div className=" saturate-0  text-neutral-50  bg-[#959595] rounded-[9px] w-64 shrink-0 drop-shadow-[4px_4px_4px_rgba(0,0,0,0.25)]  m-10">
+      <div className=" styleQ h-[301px] overflow-hidden">
+        <Image className="rounded-t-[9px] 	" src={`/${img}`} alt={"event-img"} width={256} height={301} />
+      </div>
+
+      <div className="grid grid-cols-3 ">
+        <div className="text ml-5 col-span-2  text-xl font-mono  font-extrabold ">
+          {name}
+        </div>
+        <button className="hover:bg-[#526595] col-span-1 w-63 h-22 mr-4 rounded-[10px]   text-center text-sm font-mono bg-custom-blue">
+          INFO
+        </button>
+      </div>
+      <div>
+        <hr className="w-52 ml-5 mt-1"></hr>
+      </div>
+
+      <div className=" grid grid-cols-12 ">
+        <div className="flex col-span-5">
+        <Image
+          className="mt-1"
+          src={"/Pin_fill.svg"}
+          alt={"pin-fill"}
+          width={47}
+          height={47}
+        />
+        <div className="text-xs font-khand font-semibold my-auto ">
+          {location}
+        </div>
+        </div>
+        <div className="col-span-1 ml-4 border-r-[1px] border-solid border-white h-10 w-1"></div>
+        <div className=" ml-1 flex col-span-6">
+        <Image
+          src={"/Time_fill.svg"}
+          alt={"time-fill"}
+          width={30}
+          height={24}
+        />
+        <div className="flex flex-col mr-auto my-auto ml-3 text-xs font-khand font-semibold">
+          
+            {date}
+          
+        </div>
+        </div>
+      </div>
+    </div>
+    </div>
+  )
+}
