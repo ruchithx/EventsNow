c186c936921aee3321d61583b2ebe375cabb3902
Arkambish <arkambhm.21@uom.lk>
2024-01-02 23:42:42 +0530

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
 import Login from "@/components/Login";
 import LogIn from "./user/login/page";
+import SignupSocial from "@/components/SignupSocial";
+import LoginForm from "./user/login/LoginForm";
+import SignUpFormBasic from "./user/signup/SignUpFormBasic";
 
 export default function Home() {
   return (
