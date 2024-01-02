dfbcf2d8d68f0ca062719de961a13f719bb27eee
ashandilz <ashandilsara8@gmail.com>
2024-01-02 12:50:01 +0530

    signup page done

--- app/user/signup/page.js
+++ app/user/signup/page.js
@@ -2,32 +2,36 @@ import SignUpFormBasic from "@/app/user/signup/SignUpFormBasic";
 import SignupSocial from "@/components/SignupSocial";
 import Image from "next/image";
 import Terms from "@/components/Terms";
+import LoginFormBasic from "@/components/Loginform";
+import Footer from "@/components/Footer";
 
 function page() {
   return (
     <div>
-      <div className="md:grid   md:grid-cols-2  ">
-        <div className="grid grid-cols-6">
-          <div className="col-span-1"></div>
-          <div className="col-span-4 ">
-            <div className="grid grid-cols-8">
-              <div></div>
-              <div className="col-span-6">
-                <SignUpFormBasic />
-              </div>
-              <div></div>
+      <div className="md:grid   md:grid-cols-2  " >
+                <div className="grid  grid-cols-6 col-span-1" >
+                <div className="col-span-1"></div>
+                <div className="col-span-4 ">
+
+                    <div className="grid grid-cols-8">
+                        <div></div>
+                    <div className="col-span-6"><SignUpFormBasic /></div>
+                    <div></div>
+                    </div>
+                <div className="flex justify-center">
+                <SignupSocial />
+                </div>
+                <Terms />
+                </div>
+                <div className="col-span-1"></div>
+                </div>
+                <div className=" max-md:hidden  md:col-span-1  overflow-hidden	bg-[url('/login.jpg')] bg-no-repeat bg-cover  bg-center">
+                    {/* <Image src={'/login.jpg'} alt={"event-img"} height={1} width={10000} /> */}
+                    
+                </div>
             </div>
-            <div className="flex justify-center">
-              <SignupSocial />
-            </div>
-            <Terms />
-          </div>
-          <div className="col-span-1"></div>
-        </div>
-        <div className=" invisible md:visible col-span-1 overflow-hidden h-auto	">
-          <Image src={"/login.jpg"} alt={"event-img"} width={1000} height={1} />
-        </div>
-      </div>
+            <Footer/>
+   
     </div>
   );
 }
