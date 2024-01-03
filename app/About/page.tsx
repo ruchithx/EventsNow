c92dab467bc3cff32912951f27b740b40a45ab50
Yasundara G.A.O <88391369+OshadhiYasundara@users.noreply.github.com>
2024-01-04 01:00:16 +0530

    start

---
+++ app/About/page.tsx
@@ -0,0 +1,12 @@
+import NavBar from '@/components/NavBar'
+import React from 'react'
+import Footer from '@/components/Footer'
+
+export default function page() {
+  return (
+    <div><NavBar/>
+    ashan
+    hi adasd asd sd 
+    <Footer /></div>
+  )
+}
