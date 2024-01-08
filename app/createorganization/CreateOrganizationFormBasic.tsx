7d4586b1c9a59df23bbf850cf79ab5e5140f589d
ashandilz <ashandilsara8@gmail.com>
2024-01-08 22:10:02 +0530

    have to validate using zod

--- app/createorganization/CreateOrganizationFormBasic.tsx
+++ app/createorganization/CreateOrganizationFormBasic.tsx
@@ -2,17 +2,76 @@
 import React, { useState } from 'react'
 import 'react-phone-number-input/style.css'
 import PhoneInput from 'react-phone-number-input'
+
+import { error } from "../../util/Toastify";
 import {Dropdown, DropdownTrigger, DropdownMenu, DropdownItem} from '@nextui-org/dropdown';
+
 
 export default function CreateOrganizationFormBasic() {
     const [fullName,setFullName] = useState('');
-    const [number,setnumber]= useState('');
+    const [number,setNumber]= useState('');
     const [numberType,setNumberType]=useState('');
+    const [companyName,setCompanyName]=useState('');
     const [address,setAddress] = useState('');
-    const [phoneNumber,setPhoneNumber]=useState();
+    const [phoneNumber,setPhoneNumber]=useState("");
     const [email,setEmail] =useState('');
+    
 async function sendOrganizationData(){
+  try {
+    const data = {
+      fullName,
+    numberType,
+    number,
+    companyName,
+    address,
+    phoneNumber,
+    email
+    };
+    
 
+    // if (!firstName || !lastName || !email || !password || !passwordConfirm) {
+    //   error("Please fill the form");
+    //   return;
+    // }
+
+    
+
+    const organization = await fetch("http://localhost:3000/api/v1/createOrganization/exist", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email }),
+    });
+    // console.log(organization.ok);
+    if (organization.ok) {
+      error("Already exist a organization for this email");
+      return;
+    }
+
+    const res = await fetch("http://localhost:3000/api/v1/createOrganization", {
+      method: "POST",
+      mode: "cors",
+      body: JSON.stringify(data),
+    });
+
+    if (!res.ok) {
+      error("There is a error for registartion");
+      return;
+    }
+
+    setFullName("");
+    setNumberType("");
+    setNumber("");
+    setCompanyName("");
+    setAddress("");
+    setPhoneNumber("");
+    setEmail("");
+    
+    
+  } catch (e) {
+    console.log(e);
+  }
 }
 
   return (
@@ -32,20 +91,22 @@ async function sendOrganizationData(){
             id="fullName"
             value={fullName}
             onChange={(e) => setFullName(e.target.value)}
-            className=" my-5 w-full h-8 block flex-1  bg-transparent py-1.5 text-gray-900 placeholder:text-[#455273] focus:outline-custom-orange sm:text-sm sm:leading-6 border-2 rounded-[12px] pl-4"
+            className=" my-5 w-full h-8 block flex-1  bg-transparent py-1.5 text-gray-900 placeholder:text-gray-400 focus:outline-custom-orange sm:text-sm sm:leading-6 border-2 rounded-[12px] pl-4"
             placeholder="Enter your full name  "
           ></input>
           <div className='flex gap-2'>
           <Dropdown>
       <DropdownTrigger>
-        <button className="  h-8 text-center justify-center  bg-transparent   px-4 text-[#455273]  sm:text-sm sm:leading-6 border-2 rounded-[12px] pl-4"
+        <button className="  h-8 text-center justify-center  bg-transparent   px-4 text-gray-400  sm:text-sm sm:leading-6 border-2 rounded-[12px] pl-4 pr-2"
           
         >
           
       {numberType.length > 0 ? (
         <span>{numberType} :-</span>
       ) : (
-        <span>Type</span>
+        <span className='flex'>Type<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+        <path d="M18 9L12 15L6 9" stroke="#222222"/>
+      </svg></span>
       )} 
     
         </button>
@@ -65,8 +126,8 @@ async function sendOrganizationData(){
             id="fullName"
             value={number}
             disabled = {numberType.length==0}
-            onChange={(e) => setnumber(e.target.value)}
-            className="  w-full  block flex-1  bg-transparenttext-gray-900 placeholder:text-[#455273] focus:outline-custom-orange sm:text-sm sm:leading-6 border-2 rounded-[12px] pl-4"
+            onChange={(e) => setNumber(e.target.value)}
+            className="  w-full  block flex-1  bg-transparenttext-gray-900 placeholder:text-gray-400 focus:outline-custom-orange sm:text-sm sm:leading-6 border-2 rounded-[12px] pl-4"
             placeholder={numberType.length > 0 ? ` Enter  ${numberType} number`: (
                 'Select the identify card type that you have'
               )}
@@ -76,19 +137,34 @@ async function sendOrganizationData(){
           <input
             required
             type="text"
+            name="companyName"
+            id="companyName"
+            value={companyName}
+            onChange={(e) => setCompanyName(e.target.value)}
+            className=" my-5 w-full h-8 block flex-1  bg-transparent py-1.5 text-gray-900 placeholder:text-gray-400 focus:outline-custom-orange sm:text-sm sm:leading-6 border-2 rounded-[12px] pl-4"
+            placeholder="Company Name  "
+          ></input>
+          <input
+            required
+            type="text"
             name="address"
             id="address"
             value={address}
             onChange={(e) => setAddress(e.target.value)}
-            className=" my-5 w-full h-8 block flex-1  bg-transparent py-1.5 text-gray-900 placeholder:text-[#455273] focus:outline-custom-orange sm:text-sm sm:leading-6 border-2 rounded-[12px] pl-4"
+            className=" my-5 w-full h-8 block flex-1  bg-transparent py-1.5 text-gray-900 placeholder:text-gray-400 focus:outline-custom-orange sm:text-sm sm:leading-6 border-2 rounded-[12px] pl-4"
             placeholder="Address  "
           ></input>
-<PhoneInput
-className="  w-full h-8 block flex-1  bg-transparenttext-gray-900 placeholder:text-[#455273] focus:outline-custom-orange sm:text-sm sm:leading-6 border-2 rounded-[12px] pl-4"
-      placeholder="Enter phone number"
-      value={phoneNumber}
-      onChange={setPhoneNumber}
-      defaultCountry="LK"/>
+          <input
+            required
+            type="text"
+            name="address"
+            id="address"
+            value={phoneNumber}
+            onChange={(e) => setPhoneNumber(e.target.value)}
+            className=" my-5 w-full h-8 block flex-1  bg-transparent py-1.5 text-gray-900 placeholder:text-gray-400 focus:outline-custom-orange sm:text-sm sm:leading-6 border-2 rounded-[12px] pl-4"
+            placeholder="Phone number "
+          ></input>
+
 
       
 <input
@@ -98,7 +174,7 @@ className="  w-full h-8 block flex-1  bg-transparenttext-gray-900 placeholder:te
             id="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-            className=" my-5 w-full h-8 block flex-1  bg-transparent py-1.5 text-gray-900 placeholder:text-[#455273] focus:outline-custom-orange sm:text-sm sm:leading-6 border-2 rounded-[12px] pl-4"
+            className=" my-5 w-full h-8 block flex-1  bg-transparent py-1.5 text-gray-900 placeholder:text-gray-400 focus:outline-custom-orange sm:text-sm sm:leading-6 border-2 rounded-[12px] pl-4"
             placeholder="Enter email address "
           ></input>
 
