f9dce4a2243d09423f3001b3b26baf0ab1c0291b
Vimodya <chamodivimodya@gmail.com>
2024-01-08 11:24:07 +0800

    superAdminDashboard-c

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,22 @@
 "use client";
 import NavBar from "@/components/NavBar";
 import ProfileSideNavBar from "@/components/ProfileSideNavBar";
+
+import Upcoming_Events from "@/components/Upcoming_Events";
 export default function Home() {
   return (
     <div>
       <NavBar />
       <ProfileSideNavBar />
+      <Upcoming_Events
+        EventName="Nadagama"
+        image={"image 1.png"}
+        Location="Matara"
+        Date="12.05.2024"
+        Time="23.00"
+        Ratings={"2.45"}
+        buttonDesc={"Info"}
+      />
     </div>
   );
 }
