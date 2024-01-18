b2b2550024698a36287b203e7d5f6a84d4a2f790
ruchith <124671914+ruchithx@users.noreply.github.com>
2024-01-18 13:03:10 +0530

    changers

--- components/InviteButton.tsx
+++ components/InviteButton.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import SearchIcon from "@mui/icons-material/Search";
+
 export default function InviteButton() {
   return (
     <div className="flex gap-10 ">
       <input
         type="text"
         placeholder={` Search by email `}
-        className="border-2 w-1/2 rounded-lg border-zinc-100 focus:outline-none focus:ring focus:ring-violet-100	"
+        className="border-2  rounded-lg border-zinc-100 focus:outline-none focus:ring focus:ring-violet-100	"
       />
       <button className={`bg-custom-green p-1 w-20 rounded`}>
         <div className="flex text-white text-sm font-bold justify-center">
