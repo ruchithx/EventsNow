bb14024addc725e1527a6363078bea783a5813e1
Yasundara G.A.O <88391369+OshadhiYasundara@users.noreply.github.com>
2024-01-20 15:58:07 +0530

    upcomingEvent#update

--- app/page.tsx
+++ app/page.tsx
@@ -6,14 +6,22 @@ import Org_RequestHandle from "@/components/Org_RequestHandle";
 import ProfileSettings from "@/components/ProfileSettings";
 import ProfileSideNavBar from "@/components/ProfileSideNavBar";
 import LinearIndeterminate from "@/components/Spinner";
+import UpcomingEvents from "@/components/UpcomingEvents";
 
 
 export default function Home() {
   return (
     <div>
-      <NavBar />
+      <UpcomingEvents
+        EventName ="NADAGAMA "
+        Location = "Anuradhapura"
+        Time = "11.00pm"
+        Date = "2024/12/3"
+        Ratings = "4"
+        image = "image 1.png"
+        buttonDesc = "Info"
+      />
 
-      
 
     </div>
   );
