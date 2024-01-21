e38867f89d59d65085fc6fe6763e9e628170e0bb
Yasundara G.A.O <88391369+OshadhiYasundara@users.noreply.github.com>
2024-01-21 12:56:02 +0530

    commit#2

--- app/page.tsx
+++ app/page.tsx
@@ -8,12 +8,22 @@ import ProfileSettings from "@/components/ProfileSettings";
 import ProfileSideNavBar from "@/components/ProfileSideNavBar";
 import LinearIndeterminate from "@/components/Spinner";
 import Upcoming_Events from "@/components/Upcoming_Events";
+import UpcomingEvents from "@/components/UpcomingEvents"
 
 export default function Home() {
   return (
     <div>
       <NavBar />
       <ProfileSideNavBar />
+      {/* <UpcomingEvents
+      EventName = "NADAGAMA"
+      Location = "Anjradhapura"
+      Time =" 12"
+      Date="32/587/3"
+      Ratings = "3"
+      image = "image 1.png"
+      buttonDesc = "info"
+      /> */}
     </div>
   );
 }
