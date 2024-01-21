5ec86a1fa30f5ed3b86b40ace2604c13181b01d0
Arkambish <arkambhm.21@uom.lk>
2024-01-21 21:31:33 +0530

    button animation

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,24 +1,39 @@
 import Image from "next/image";
-import React from "react";
-interface log {
-  titleOfbutton: String;
-  image: String;
-  fn: React.MouseEventHandler<HTMLButtonElement>;
+import React, { useState } from "react";
+import styles from "./Login.module.css";
+
+interface LogProps {
+  titleOfButton: string;
+  image: string;
+  fn?: React.MouseEventHandler<HTMLButtonElement>;
 }
 
-export default function Login({ image, titleOfbutton, fn }: log) {
+export default function Login({ image, titleOfButton, fn }: LogProps) {
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setIsClicked(!isClicked);
+
+    if (fn) {
+      fn(event);
+    }
+  };
+
   return (
-    <button onClick={fn} className={`bg-custom-orange w-28 p-2 rounded-2xl`}>
-      <div className="flex flex-row ml-1 justify-center">
+    <button
+      onClick={handleClick}
+      className={`bg-custom-orange h-8 rounded-2xl ${
+        isClicked ? styles.clicked : ""
+      }`}
+    >
+      <div className="flex flex-row ml-2 mr-2 gap-2 p-0 items-center justify-center">
         <Image
           src={`/ReUsableComponentData/${image}`}
           alt="Picture of the button"
           width={20}
           height={20}
         />
-        <div className=" text-white text-sm font-bold ml-2">
-          {titleOfbutton}
-        </div>
+        <div className="text-white text-sm font-bold">{titleOfButton}</div>
       </div>
     </button>
   );
