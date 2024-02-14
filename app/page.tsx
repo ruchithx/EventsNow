296409a72ee59d2bcc6cd6d620bb18932c29eaa7
Yasundara G.A.O <88391369+OshadhiYasundara@users.noreply.github.com>
2024-02-15 01:10:11 +0530

    #CreateComponent_2/15

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,20 @@
 "use client";
 
 import Footer from "@/components/Footer";
+import Event from "./event/dashboard/[id]/components/Event";
 
 export default function Home() {
   return (
-    <div className="">
-      <Footer />
+    <div className="p-12">
+      {/* <Footer /> */}
+      <Event 
+        EventName={"NadaGama"}
+        Location={"Anuradhapura"}
+        Date={"21st June 2024"}
+        Time={"16.00 t0 22.00"}
+        buttonDesc={"Stein Studio"}
+
+      />
     </div>
   );
 }
