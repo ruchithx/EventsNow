e19c4512af2606abe7e3364cb755dcc3e9a7238a
ashandilz <ashandilsara8@gmail.com>
2024-02-22 03:31:21 +0530

    change middle component implemented

---
+++ app/event/dashboard/[id]/components/Campaign.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Campaign() {
+  return <div>campaign</div>;
+}
---
+++ app/event/dashboard/[id]/components/Overview.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Overview() {
+  return (
+    <div>
+      overview
+    </div>
+  )
+}
