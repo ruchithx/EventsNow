5ee29616a3aa436db7f9c9eee638b401e2bbebf8
ruchith <124671914+ruchithx@users.noreply.github.com>
2024-02-22 14:11:32 +0530

    changers

--- app/createorganization/page.tsx
+++ app/createorganization/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import NavBar from "@/components/Navbar/NavBar";
+
 import Footer from "@/components/Footer";
 import CreateOrganizationFormBasic from "./components/CreateOrganizationFormBasic";
 
