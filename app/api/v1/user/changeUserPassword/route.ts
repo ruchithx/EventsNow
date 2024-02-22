63ec99dafb5a71611bd0be58e642e18b496f02a2
ruchith <124671914+ruchithx@users.noreply.github.com>
2024-02-22 11:24:02 +0530

    changers

--- app/api/v1/user/changeUserPassword/route.ts
+++ app/api/v1/user/changeUserPassword/route.ts
@@ -1,12 +1,11 @@
 import connectMongoDB from "@/lib/mongo/mongodb";
 import User from "@/models/userModel";
-import mongoose from "mongoose";
 import { NextResponse } from "next/server";
 import bcrypt from "bcrypt";
 
 async function updateUserPassword(userId: string, newPassword: string) {
   try {
-    await connectMongoDB(); // Ensure database connection is established
+    const db = await connectMongoDB(); // Ensure database connection is established
 
     const user = await User.findById(userId);
     if (!user) {
@@ -23,26 +22,30 @@ async function updateUserPassword(userId: string, newPassword: string) {
 
 export async function POST(req: Request) {
   try {
-    await connectMongoDB(); // Ensure database connection is established
+    const db = await connectMongoDB(); // Ensure database connection is established
     const { id, currentPassword, newPassword } = await req.json();
 
     const user = await User.findById(id);
     if (!user) {
-      return { error: "User not found" }; // Return an error response
+      return NextResponse.json({ error: "User not found" }); // Return an error response
     }
 
     const passwordMatch = await bcrypt.compare(currentPassword, user.password);
     if (!passwordMatch) {
-      return { error: "Incorrect Password" }; // Return an error response
+      return NextResponse.json({ error: "Incorrect Password" });
+      // Return an error response
     }
 
     const hashedPassword = await bcrypt.hash(newPassword, 10);
     await updateUserPassword(id, hashedPassword);
 
     console.log("Password changed successfully");
-    return { success: true, message: "Password changed successfully" }; // Return success response
+    return NextResponse.json({
+      success: true,
+      message: "Password changed successfully",
+    });
   } catch (error: any) {
     console.error("Connection failed:", error.message);
-    return { error: "Connection failed" }; // Return an error response
+    return NextResponse.json({ error: "Connection failed" });
   }
 }
