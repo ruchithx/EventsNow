89ed91514c4883233817d0a945873023fd187ad6
ruchith <124671914+ruchithx@users.noreply.github.com>
2024-02-24 14:48:08 +0530

    tempory data add

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,19 @@
 "use client";
 
 import Footer from "@/components/Footer";
+import { getAllOrganization } from "./admin/dashboard/[id]/FetchData";
 
 export default function Home() {
+  async function handleButton() {
+    const res = await getAllOrganization();
+    const data = await res.json();
+    console.log(data);
+  }
   return (
     <div>
+      <button className="w-40 bg-black text-white" onClick={handleButton}>
+        click the button{" "}
+      </button>
       <Footer />
     </div>
   );
