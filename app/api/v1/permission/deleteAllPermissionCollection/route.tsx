eacd0288bc7023940ecf7ab590fd42956b33e9e7
ruchith <124671914+ruchithx@users.noreply.github.com>
2024-02-24 16:55:49 +0530

    final changers

--- app/api/v1/permission/deleteAllPermissionCollection/route.tsx
+++ app/api/v1/permission/deleteAllPermissionCollection/route.tsx
@@ -1,13 +1,14 @@
 import connectMongoDB from "@/lib/mongo/mongodb";
 import Organization from "@/models/organizationModel";
 import Permission from "@/models/permissionModel";
+import User from "@/models/userModel";
 
 import { NextResponse } from "next/server";
 
 export async function DELETE() {
   try {
     await connectMongoDB();
-    await Permission.deleteMany();
+    // await User.deleteMany();
     return NextResponse.json({ message: "data delete success" });
   } catch (e) {
     console.log(e);
--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,13 @@
 "use client";
 
 import Footer from "@/components/Footer";
-import {
-  getAllOrganization,
-  getAllUser,
-} from "./admin/dashboard/[id]/FetchData";
 
 export default function Home() {
-  async function handleButton() {
-    const res = await getAllOrganization();
-    const data = await res.json();
-    console.log(data);
-  }
-
-  async function handleuser() {
-    const res = await getAllUser();
-    const data = await res.json();
-    console.log("user" + data);
-  }
   return (
     <div>
-      <button className="w-40 bg-black text-white" onClick={handleButton}>
-        click the organization{" "}
-      </button>
-      <button className="w-40 bg-black text-white" onClick={handleuser}>
-        click the User{" "}
-      </button>
-      <button className="w-40 bg-black text-white" onClick={handleButton}>
-        click the event{" "}
-      </button>
+      <div>Hero section</div>
+      <div>Event</div>
+      <div>Out dated Event</div>
       <Footer />
     </div>
   );
