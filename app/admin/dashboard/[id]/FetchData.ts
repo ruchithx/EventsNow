84f4478c9bc7c5bd959eef6548b79ae734d26d90
ruchith <124671914+ruchithx@users.noreply.github.com>
2024-02-24 15:16:01 +0530

    changers cache

--- app/admin/dashboard/[id]/FetchData.ts
+++ app/admin/dashboard/[id]/FetchData.ts
@@ -10,14 +10,16 @@ export const getAllOrganization = async () => {
 export const getAllUser = async () => {
   const res = await fetch(
     // `api/v1/user/getAllUser`
-    `${process.env.NEXT_PUBLIC_URL}/api/v1/user/getAllUser`
+    `${process.env.NEXT_PUBLIC_URL}/api/v1/user/getAllUser`,
+    { cache: "no-store" }
   );
   return res;
 };
 
 export const getAllEvents = async () => {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_URL}/api/v1/event/getAllEvents`
+    `${process.env.NEXT_PUBLIC_URL}/api/v1/event/getAllEvents`,
+    { cache: "no-store" }
     // `${process.env.NEXT_PUBLIC_URL}api/v1/event/getAllEvents`
   );
   return res;
