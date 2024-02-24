d674d0c67fb36ad613ee490db91d3aef6c7c0d29
ruchith <124671914+ruchithx@users.noreply.github.com>
2024-02-24 08:43:00 +0530

    api changers

--- app/api/v1/organization/getAllOrganization/route.ts
+++ app/api/v1/organization/getAllOrganization/route.ts
@@ -3,12 +3,16 @@ import Organization from "@/models/organizationModel";
 import connectMongoDB from "@/lib/mongo/mongodb";
 
 export async function GET() {
-  await connectMongoDB();
-  const organization = await Organization.find();
-  console.log("organization is", organization);
-  if (!organization) {
-    return NextResponse.json({ message: "No organization" });
-  }
+  try {
+    await connectMongoDB();
+    const organization = await Organization.find();
+    console.log("organization is", organization);
+    if (!organization) {
+      return NextResponse.json({ message: "No organization" });
+    }
 
-  return NextResponse.json({ organization });
+    return NextResponse.json({ organization });
+  } catch (e) {
+    console.log(e);
+  }
 }
