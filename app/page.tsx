4f87b23bdbfe2702a39e1589e4043dccff8e53a9
ruchith <124671914+ruchithx@users.noreply.github.com>
2024-02-25 21:36:20 +0530

    data feting

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 "use client";
 
 import Footer from "@/components/Footer";
-import { getAllOrganization } from "./admin/dashboard/[id]/FetchData";
+import {
+  getAllOrganization,
+  getAllUser,
+} from "./admin/dashboard/[id]/FetchData";
 
 export default function Home() {
   async function handleClickIt() {
