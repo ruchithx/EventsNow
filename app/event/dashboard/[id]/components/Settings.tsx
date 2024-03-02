db45ee68d9a758a7d2529c06590994f391884777
ashandilz <ashandilsara8@gmail.com>
2024-03-02 10:51:32 +0530

    setting ui ongoing

--- app/event/dashboard/[id]/components/Settings.tsx
+++ app/event/dashboard/[id]/components/Settings.tsx
@@ -3,6 +3,7 @@ import Container from "./Container";
 import Switch from "react-switch";
 import ContainerWithStroke from "./ContainerWithStroke";
 import { set } from "mongoose";
+import Image from "next/image";
 
 export default function Settings() {
   const [checked, setChecked] = useState(false);
@@ -11,6 +12,7 @@ export default function Settings() {
   };
 
   const [eventname, setEventname] = useState("nada gama");
+  const [eventLocation, setEventLocation] = useState("KCC,Kandy Road,kandy.");
   return (
     <Container>
       <div className=" ml-16 mb-5 grid gap-2 mt-8 mr-16 pb-20">
@@ -59,15 +61,24 @@ export default function Settings() {
                 <div className="w-full text-left text-lg ">Event Type</div>
                 <div className="w-full flex justify-start ">
                   <button className="">physical Event</button>
-                  <input
-                    type="text"
-                    placeholder=""
-                    onChange={(e) => {
-                      setEventname(e.target.value);
-                    }}
-                    value={eventname}
-                    className="focus:outline-custom-orange border-2 rounded-md pl-4 pr-16 placeholder:text-[#ABB7C2] placeholder:font-Inter placeholder:text-base placeholder:font-normal place-self-start w-full"
-                  />
+                  <div className="flex focus:outline-custom-orange border-2 rounded-md">
+                    <Image
+                      className="grid content-center"
+                      src="/images/eventDash/Pin_fill.svg"
+                      alt="event"
+                      width={27}
+                      height={27}
+                    />
+                    <input
+                      type="text"
+                      placeholder=""
+                      onChange={(e) => {
+                        setEventname(e.target.value);
+                      }}
+                      value={eventLocation}
+                      className=" pl-4 pr-16 placeholder:text-[#ABB7C2] placeholder:font-Inter placeholder:text-base placeholder:font-normal place-self-start w-full border-none focus:outline-none"
+                    />
+                  </div>
                 </div>
               </div>
             </ContainerWithStroke>
