97431e51e87a9ccb7731067f48de8ea68f23ba99
ashandilz <ashandilsara8@gmail.com>
2024-03-10 23:09:30 +0530

    event form update done

--- app/event/host/[id]/page.tsx
+++ app/event/host/[id]/page.tsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import SelectTemplate from "./SelectTemplate";
 
 export default function Home() {
-  console.log("hello1");
   return (
     <div>
       <SelectTemplate />
