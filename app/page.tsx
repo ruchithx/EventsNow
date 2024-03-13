cd45eed6fc1b308b9e14fec4ec337dcb7f61f37f
ruchith <124671914+ruchithx@users.noreply.github.com>
2024-03-13 22:41:54 +0530

    add getData function

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,29 @@
 import React from "react";
-
+import Footer from "@/components/Footer";
+import EventCardDisabled from "@/components/EventCardDisabled";
+import { formatDate } from "@/util/helper";
 // import EventCardDisabled from "@/components/EventCardDisabled";
 
 // import EventViewMode from "../components/EventViewMode";
 // import { Event } from "./admin/Type";
 
-// async function getData(): Promise<Event[]> {
-//   const response = await fetch(
-//     `${process.env.NEXT_PUBLIC_URL}/api/v1/event/outdatedEvents`,
-//     {
-//       method: "GET",
-//       mode: "cors",
-//     }
-//   );
-//   const data = await response.json();
-//   return data;
-// }
+async function getData() {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_URL}/api/v1/event/outdatedEvents`,
+    {
+      method: "GET",
+      mode: "cors",
+    }
+  );
+  const data = await response.json();
+  return data;
+}
 
-import Footer from "@/components/Footer";
 // import HeroSection from "@/components/HeroSection";
 // import { formatDate } from "@/util/helper";
 
 export default async function Home() {
-  // const data = await getData();
+  const data = await getData();
   return (
     <div>
       {/* <HeroSection />
@@ -31,18 +32,19 @@ export default async function Home() {
       <div className="font-bold text-[30px] md:text-[40px] lg:text-[60px] text-[#906953] drop-shadow-lg ms-8">
         Outdated Events
       </div>
+  */}
 
       <div className="flex flex-wrap ms-12">
-        {data.map((e, index) => (
+        {data.map((e: any) => (
           <EventCardDisabled
-            key={index}
+            key={e._id}
             name={e.eventName}
             img={e.postImageLink}
             location={e.selectedTab}
             date={formatDate(e.eventStartDate)}
           />
         ))}
-      </div> */}
+      </div>
 
       <Footer />
     </div>
